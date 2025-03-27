63492b1b676447c9a4d456278fd039e388168556
cloudypie <thomasgwu49@gmail.com>
2025-03-27 00:50:34 -0400

    got rid of src

--- js/skeletonManager.js
+++ js/skeletonManager.js
@@ -10,56 +10,56 @@ class navBar extends HTMLElement {
                 <!--<div class="dropdown">
                       <button onclick="openCC()" class="dropbtn10">Credit Cards</button>
                       <div id="myCC1" class="dropdown-content">
-                          <a href="/src/Credit_Card/Amex/">American Express</a>
-                          <a href="/src/Credit_Card/BoA/">Bank of America</a>
-                          <a href="/src/Credit_Card/C1/">Capital One</a>
-                          <a href="/src/Credit_Card/Chase/">Chase</a>
-                          <a href="/src/Credit_Card/Citi/">Citi</a>
-                          <a href="/src/Credit_Card/USBank/">US Bank</a>
-                          <a href="/src/Credit_Card/WF/">Wells Fargo</a>
-                          <a href="/src/Credit_Card/Other/">Other</a>
+                          <a href="/Credit_Card/Amex/">American Express</a>
+                          <a href="/Credit_Card/BoA/">Bank of America</a>
+                          <a href="/Credit_Card/C1/">Capital One</a>
+                          <a href="/Credit_Card/Chase/">Chase</a>
+                          <a href="/Credit_Card/Citi/">Citi</a>
+                          <a href="/Credit_Card/USBank/">US Bank</a>
+                          <a href="/Credit_Card/WF/">Wells Fargo</a>
+                          <a href="/Credit_Card/Other/">Other</a>
                       </div>
                   </div> -->
 
                 <div class="dropdown">
                     <button onclick="openAirline()" class="dropbtn11">Airlines</button>
                     <div id="myAirline1" class="dropdown-content">
-                          <!-- <a href="/src/Airlines/Air_Canada_Guide/">Air Canada</a>-->
-                          <!-- <a href="/src/Airlines/Air_France_KLM_Guide">Air France/KLM</a>-->
-                          <!-- <a href="/src/Airlines/Alaska_Airlines_Guide/">Alaska</a>-->
-                          <!-- <a href="/src/Airlines/American_Airlines_Guide/">American</a>-->
-                          <a href="/src/Airlines/Delta_Airlines_Guide/">Delta</a>
-                          <!-- <a href="/src/Airlines/Frontier_Airlines_Guide/">Frontier</a>-->
-                          <!-- <a href="/src/Airlines/Qatar_Airways_Guide/">Qatar Airways</a>-->
-                          <!-- <a href="/src/Airlines/Southwest_Airlines_Guide/">Southwest</a>-->
-                          <!-- <a href="/src/Airlines/Starlux_Airlines_Guide/">Starlux</a>-->
-                          <!-- <a href="/src/Airlines/United_Airlines_Guide/">United</a>-->
+                          <!-- <a href="/Airlines/Air_Canada_Guide/">Air Canada</a>-->
+                          <!-- <a href="/Airlines/Air_France_KLM_Guide">Air France/KLM</a>-->
+                          <!-- <a href="/Airlines/Alaska_Airlines_Guide/">Alaska</a>-->
+                          <!-- <a href="/Airlines/American_Airlines_Guide/">American</a>-->
+                          <a href="/Airlines/Delta_Airlines_Guide/">Delta</a>
+                          <!-- <a href="/Airlines/Frontier_Airlines_Guide/">Frontier</a>-->
+                          <!-- <a href="/Airlines/Qatar_Airways_Guide/">Qatar Airways</a>-->
+                          <!-- <a href="/Airlines/Southwest_Airlines_Guide/">Southwest</a>-->
+                          <!-- <a href="/Airlines/Starlux_Airlines_Guide/">Starlux</a>-->
+                          <!-- <a href="/Airlines/United_Airlines_Guide/">United</a>-->
                       </div>
                   </div>
 
                 <div class="dropdown">
                   <button onclick="openHotel()" class="dropbtn12">Hotels</button>
                   <div id="myHotel1" class="dropdown-content">
-                      <!-- <a href="/src/Hotels/Choice/">Choice</a>-->
-                      <a href="/src/Hotels/Hilton_Guide/">Hilton</a>
-                      <!-- <a href="/src/Hotels/Hyatt_Guide/">Hyatt</a>-->
-                      <!-- <a href="/src/Hotels/IHG_Guide/">IHG</a>-->
-                      <!-- <a href="/src/Hotels/Marriott_Guide/">Marriott</a>-->
-                      <!-- <a href="/src/Hotels/Wyndham_Guide/">Wyndham</a>-->
+                      <!-- <a href="/Hotels/Choice/">Choice</a>-->
+                      <a href="/Hotels/Hilton_Guide/">Hilton</a>
+                      <!-- <a href="/Hotels/Hyatt_Guide/">Hyatt</a>-->
+                      <!-- <a href="/Hotels/IHG_Guide/">IHG</a>-->
+                      <!-- <a href="/Hotels/Marriott_Guide/">Marriott</a>-->
+                      <!-- <a href="/Hotels/Wyndham_Guide/">Wyndham</a>-->
                   </div>
                 </div>
 
                 <div class="dropdown">
                     <button onclick="openSS()" class="dropbtn13">Sweet Spots</button>
                     <div id="mySS1" class="dropdown-content">
-                      <a href="/src/Sweet_Spots/Domestic/">Domestic</a>
+                      <a href="/Sweet_Spots/Domestic/">Domestic</a>
                     </div>
                 </div>
                   
 
-                  <!-- <a href="/src/Tierlist/">Tierlist</a> -->
-                  <!-- <a href="/src/Travel_Review/">Travel Reviews</a> -->
-                  <!-- <a href="/src/News_Archive/">News Archive</a> -->
+                  <!-- <a href="/Tierlist/">Tierlist</a> -->
+                  <!-- <a href="/Travel_Review/">Travel Reviews</a> -->
+                  <!-- <a href="/News_Archive/">News Archive</a> -->
               </div>
 
               <div class="sidenavbtn">
@@ -73,60 +73,60 @@ class navBar extends HTMLElement {
 
               <!-- Desktop Navigation Bar-->
               <a href="/">  
-                  <img src="/src/assets/LogoNav.png" alt="Worldly Express">
+                  <img src="/assets/LogoNav.png" alt="Worldly Express">
               </a>
               <nav>
                   <!--<div class="dropdown">
                       <button onclick="myFunction()" class="dropbtn">Credit Cards</button>
                       <div class="dropdown-content">
-                          <a href="/src/Credit_Card/Amex/">American Express</a>
-                          <a href="/src/Credit_Card/BoA/">Bank of America</a>
-                          <a href="/src/Credit_Card/C1/">Capital One</a>
-                          <a href="/src/Credit_Card/Chase/">Chase</a>
-                          <a href="/src/Credit_Card/Citi/">Citi</a>
-                          <a href="/src/Credit_Card/USBank/">US Bank</a>
-                          <a href="/src/Credit_Card/WF/">Wells Fargo</a>
-                          <a href="/src/Credit_Card/Other/">Other</a>
+                          <a href="/Credit_Card/Amex/">American Express</a>
+                          <a href="/Credit_Card/BoA/">Bank of America</a>
+                          <a href="/Credit_Card/C1/">Capital One</a>
+                          <a href="/Credit_Card/Chase/">Chase</a>
+                          <a href="/Credit_Card/Citi/">Citi</a>
+                          <a href="/Credit_Card/USBank/">US Bank</a>
+                          <a href="/Credit_Card/WF/">Wells Fargo</a>
+                          <a href="/Credit_Card/Other/">Other</a>
                       </div>
                   </div> -->
           
                   <div class="dropdown">
                       <button onclick="myFunction1()" class="dropbtn1">Airlines</button>
                       <div id="myAirline" class="dropdown-content">
-                          <!-- <a href="/src/Airlines/Air_Canada_Guide/">Air Canada</a>-->
-                          <!-- <a href="/src/Airlines/Air_France_KLM_Guide">Air France/KLM</a>-->
-                          <!-- <a href="/src/Airlines/Alaska_Airlines_Guide/">Alaska</a>-->
-                          <!-- <a href="/src/Airlines/American_Airlines_Guide/">American</a>-->
-                          <a href="/src/Airlines/Delta_Airlines_Guide/">Delta</a>
-                          <!-- <a href="/src/Airlines/Frontier_Airlines_Guide/">Frontier</a>-->
-                          <!-- <a href="/src/Airlines/Qatar_Airways_Guide/">Qatar Airways</a>-->
-                          <!-- <a href="/src/Airlines/Southwest_Airlines_Guide/">Southwest</a>-->
-                          <!-- <a href="/src/Airlines/Starlux_Airlines_Guide/">Starlux</a>-->
-                          <!-- <a href="/src/Airlines/United_Airlines_Guide/">United</a>-->
+                          <!-- <a href="/Airlines/Air_Canada_Guide/">Air Canada</a>-->
+                          <!-- <a href="/Airlines/Air_France_KLM_Guide">Air France/KLM</a>-->
+                          <!-- <a href="/Airlines/Alaska_Airlines_Guide/">Alaska</a>-->
+                          <!-- <a href="/Airlines/American_Airlines_Guide/">American</a>-->
+                          <a href="/Airlines/Delta_Airlines_Guide/">Delta</a>
+                          <!-- <a href="/Airlines/Frontier_Airlines_Guide/">Frontier</a>-->
+                          <!-- <a href="/Airlines/Qatar_Airways_Guide/">Qatar Airways</a>-->
+                          <!-- <a href="/Airlines/Southwest_Airlines_Guide/">Southwest</a>-->
+                          <!-- <a href="/Airlines/Starlux_Airlines_Guide/">Starlux</a>-->
+                          <!-- <a href="/Airlines/United_Airlines_Guide/">United</a>-->
                       </div>
                   </div>
                   
                   <div class="dropdown">
                       <button onclick="myFunction2()" class="dropbtn2">Hotels</button>
                       <div id="myHotel" class="dropdown-content">
-                          <!-- <a href="/src/Hotels/Choice/">Choice</a>-->
-                          <a href="/src/Hotels/Hilton_Guide/">Hilton</a>
-                          <!-- <a href="/src/Hotels/Hyatt_Guide/">Hyatt</a>-->
-                          <!-- <a href="/src/Hotels/IHG_Guide/">IHG</a>-->
-                          <!-- <a href="/src/Hotels/Marriott_Guide/">Marriott</a>-->
-                          <!-- <a href="/src/Hotels/Wyndham_Guide/">Wyndham</a>-->
+                          <!-- <a href="/Hotels/Choice/">Choice</a>-->
+                          <a href="/Hotels/Hilton_Guide/">Hilton</a>
+                          <!-- <a href="/Hotels/Hyatt_Guide/">Hyatt</a>-->
+                          <!-- <a href="/Hotels/IHG_Guide/">IHG</a>-->
+                          <!-- <a href="/Hotels/Marriott_Guide/">Marriott</a>-->
+                          <!-- <a href="/Hotels/Wyndham_Guide/">Wyndham</a>-->
                       </div>
                   </div>
 
                   <div class="dropdown">
                       <button onclick="myFunction3()" class="dropbtn3">Sweet Spots</button>
                       <div id="mySS" class="dropdown-content">
-                          <a href="/src/Sweet_Spots/Domestic/">Domestic</a>
+                          <a href="/Sweet_Spots/Domestic/">Domestic</a>
                       </div>
                   </div>
-                  <!-- <a href="/src/Tierlist">Tierlist</a> -->
-                  <!-- <a href="/src/Travel_Review">Travel Reviews</a> -->
-                  <!-- <a href="/src/Concierge">Concierge</a> -->
+                  <!-- <a href="/Tierlist">Tierlist</a> -->
+                  <!-- <a href="/Travel_Review">Travel Reviews</a> -->
+                  <!-- <a href="/Concierge">Concierge</a> -->
               </nav>
             </header>
         `;
