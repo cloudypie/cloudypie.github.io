93c4813c90134ca14ba13b81ea29cbd181552f63
cloudypie <thomasgwu49@gmail.com>
2025-04-19 00:39:38 -0400

    mobile sidenav fix

--- js/skeletonManager.js
+++ js/skeletonManager.js
@@ -1,122 +1,212 @@
-class navBar extends HTMLElement{connectedCallback(){this.innerHTML=`
-  <header>
-    <!-- Mobile Navigation Bar-->
-    <div id="mySidenav" class="sidenav">
-      <button onclick="closeNav()" class="closebtn">&times;</button>
-      <!--<div class="dropdown">
-            <button onclick="openCC()" class="dropbtn10">Credit Cards</button>
-            <div id="myCC1" class="dropdown-content">
-                <a href="/Credit_Card/Amex/">American Express</a>
-                <a href="/Credit_Card/BoA/">Bank of America</a>
-                <a href="/Credit_Card/C1/">Capital One</a>
-                <a href="/Credit_Card/Chase/">Chase</a>
-                <a href="/Credit_Card/Citi/">Citi</a>
-                <a href="/Credit_Card/USBank/">US Bank</a>
-                <a href="/Credit_Card/WF/">Wells Fargo</a>
-                <a href="/Credit_Card/Other/">Other</a>
-            </div>
-        </div> -->
+class navBar extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+            <header>
+            <!-- Mobile Navigation Bar-->
+            <div id="mySidenav" class="sidenav">
+              <ul>
+                <li>
+                  <button onclick="closeNav()" class="closebtn">&times;</button>
+                </li>
 
-      <div class="dropdown">
-          <button onclick="openAirline()" class="dropbtn11">Airlines</button>
-          <div id="myAirline1" class="dropdown-content">
-                <!-- <a href="/Airlines/Air_Canada_Guide/">Air Canada</a>-->
-                <!-- <a href="/Airlines/Air_France_KLM_Guide">Air France/KLM</a>-->
-                <!-- <a href="/Airlines/Alaska_Airlines_Guide/">Alaska</a>-->
-                <!-- <a href="/Airlines/American_Airlines_Guide/">American</a>-->
-                <a href="/Airlines/Delta_Airlines_Guide/">Delta</a>
-                <!-- <a href="/Airlines/Frontier_Airlines_Guide/">Frontier</a>-->
-                <!-- <a href="/Airlines/Qatar_Airways_Guide/">Qatar Airways</a>-->
-                <!-- <a href="/Airlines/Southwest_Airlines_Guide/">Southwest</a>-->
-                <!-- <a href="/Airlines/Starlux_Airlines_Guide/">Starlux</a>-->
-                <!-- <a href="/Airlines/United_Airlines_Guide/">United</a>-->
-            </div>
-        </div>
+                <!-- <li>
+                  <button onclick=toggleSubMenu(this) class="dropbtn">Credit Cards</button>
+                  <ul class="sub-menu">
+                    <div>
+                      <a href="/Credit_Card/Amex/">American Express</a>
+                      <a href="/Credit_Card/BoA/">Bank of America</a>
+                      <a href="/Credit_Card/C1/">Capital One</a>
+                      <a href="/Credit_Card/Chase/">Chase</a>
+                      <a href="/Credit_Card/Citi/">Citi</a>
+                      <a href="/Credit_Card/USBank/">US Bank</a>
+                      <a href="/Credit_Card/WF/">Wells Fargo</a>
+                      <a href="/Credit_Card/Other/">Other</a>
+                    </div>
+                  </ul>
+                </li> -->
 
-      <div class="dropdown">
-        <button onclick="openHotel()" class="dropbtn12">Hotels</button>
-        <div id="myHotel1" class="dropdown-content">
-            <!-- <a href="/Hotels/Choice/">Choice</a>-->
-            <a href="/Hotels/Hilton_Guide/">Hilton</a>
-            <!-- <a href="/Hotels/Hyatt_Guide/">Hyatt</a>-->
-            <!-- <a href="/Hotels/IHG_Guide/">IHG</a>-->
-            <!-- <a href="/Hotels/Marriott_Guide/">Marriott</a>-->
-            <!-- <a href="/Hotels/Wyndham_Guide/">Wyndham</a>-->
-        </div>
-      </div>
+                <li>
+                  <button onclick=toggleSubMenu(this) class="dropbtn">Airlines</button>
+                  <ul class="sub-menu">
+                    <div>
+                      <!-- <a href="/Airlines/Air_Canada_Guide/">Air Canada</a>-->
+                      <!-- <a href="/Airlines/Air_France_KLM_Guide">Air France/KLM</a>-->
+                      <!-- <a href="/Airlines/Alaska_Airlines_Guide/">Alaska</a>-->
+                      <!-- <a href="/Airlines/American_Airlines_Guide/">American</a>-->
+                      <li><a href="/Airlines/Delta_Airlines_Guide/">Delta</a></li>
+                      <!-- <a href="/Airlines/Frontier_Airlines_Guide/">Frontier</a>-->
+                      <!-- <a href="/Airlines/Qatar_Airways_Guide/">Qatar Airways</a>-->
+                      <!-- <a href="/Airlines/Southwest_Airlines_Guide/">Southwest</a>-->
+                      <!-- <a href="/Airlines/Starlux_Airlines_Guide/">Starlux</a>-->
+                      <!-- <a href="/Airlines/United_Airlines_Guide/">United</a>-->
+                    </div>
+                  </ul>
+                </li>
 
-      <div class="dropdown">
-          <button onclick="openSS()" class="dropbtn13">Sweet Spots</button>
-          <div id="mySS1" class="dropdown-content">
-            <a href="/Sweet_Spots/Domestic/">Domestic</a>
-          </div>
-      </div>
-        <!-- <a href="/Tierlist/">Tierlist</a> -->
-        <!-- <a href="/Travel_Review/">Travel Reviews</a> -->
-        <!-- <a href="/News_Archive/">News Archive</a> -->
-    </div>
+                <li>
+                  <button onclick=toggleSubMenu(this) class="dropbtn">Hotels</button>
+                  <ul class="sub-menu">
+                    <div>
+                      <!-- <a href="/Hotels/Choice/">Choice</a>-->
+                      <li><a href="/Hotels/Hilton_Guide/">Hilton</a></li>
+                      <!-- <a href="/Hotels/Hyatt_Guide/">Hyatt</a>-->
+                      <!-- <a href="/Hotels/IHG_Guide/">IHG</a>-->
+                      <!-- <a href="/Hotels/Marriott_Guide/">Marriott</a>-->
+                      <!-- <a href="/Hotels/Wyndham_Guide/">Wyndham</a>-->
+                    </div>
+                  </ul>
+                </li>
 
-    <div class="sidenavbtn">
-      <!-- Use any element to open the sidenav -->
-      <span style="font-size:15vw;cursor:pointer" onclick="openNav()">&#9776;</span>
-    </div>
-    
-    <!-- Desktop Navigation Bar-->
-    <a href="/">  
-        <img src="/assets/LogoNav.webp" alt="Worldly Express">
-    </a>
-    <nav>
-        <!--<div class="dropdown">
-            <button onclick="myFunction()" class="dropbtn">Credit Cards</button>
-            <div class="dropdown-content">
-                <a href="/Credit_Card/Amex/">American Express</a>
-                <a href="/Credit_Card/BoA/">Bank of America</a>
-                <a href="/Credit_Card/C1/">Capital One</a>
-                <a href="/Credit_Card/Chase/">Chase</a>
-                <a href="/Credit_Card/Citi/">Citi</a>
-                <a href="/Credit_Card/USBank/">US Bank</a>
-                <a href="/Credit_Card/WF/">Wells Fargo</a>
-                <a href="/Credit_Card/Other/">Other</a>
+                <li>
+                  <button onclick=toggleSubMenu(this) class="dropbtn">Sweet Spots</button>
+                  <ul class="sub-menu">
+                    <div>
+                      <li><a href="/Sweet_Spots/Domestic/">Domestic</a></li>
+                    </div>
+                  </ul>
+                </li>
+                <li>
+                  <!-- <a href="/Tierlist/">Tierlist</a> -->
+                </li>
+                <li>
+                  <!-- <a href="/Travel_Review/">Travel Reviews</a> -->
+                </li>
+                <li>
+                  <!-- <a href="/News_Archive/">News Archive</a> -->
+                </li>
+              </ul>
             </div>
-        </div> -->
 
-        <div class="dropdown">
-            <button onclick="myFunction1()" class="dropbtn1">Airlines</button>
-            <div id="myAirline" class="dropdown-content">
-                <!-- <a href="/Airlines/Air_Canada_Guide/">Air Canada</a>-->
-                <!-- <a href="/Airlines/Air_France_KLM_Guide">Air France/KLM</a>-->
-                <!-- <a href="/Airlines/Alaska_Airlines_Guide/">Alaska</a>-->
-                <!-- <a href="/Airlines/American_Airlines_Guide/">American</a>-->
-                <a href="/Airlines/Delta_Airlines_Guide/">Delta</a>
-                <!-- <a href="/Airlines/Frontier_Airlines_Guide/">Frontier</a>-->
-                <!-- <a href="/Airlines/Qatar_Airways_Guide/">Qatar Airways</a>-->
-                <!-- <a href="/Airlines/Southwest_Airlines_Guide/">Southwest</a>-->
-                <!-- <a href="/Airlines/Starlux_Airlines_Guide/">Starlux</a>-->
-                <!-- <a href="/Airlines/United_Airlines_Guide/">United</a>-->
+            <div class="sidenavbtn">
+              <!-- Use any element to open the sidenav -->
+              <span style="font-size:15vw;cursor:pointer" onclick="openNav()">&#9776;</span>
             </div>
-        </div>
-        
-        <div class="dropdown">
-            <button onclick="myFunction2()" class="dropbtn2">Hotels</button>
-            <div id="myHotel" class="dropdown-content">
-                <!-- <a href="/Hotels/Choice/">Choice</a>-->
-                <a href="/Hotels/Hilton_Guide/">Hilton</a>
-                <!-- <a href="/Hotels/Hyatt_Guide/">Hyatt</a>-->
-                <!-- <a href="/Hotels/IHG_Guide/">IHG</a>-->
-                <!-- <a href="/Hotels/Marriott_Guide/">Marriott</a>-->
-                <!-- <a href="/Hotels/Wyndham_Guide/">Wyndham</a>-->
-            </div>
-        </div>
 
-        <div class="dropdown">
-            <button onclick="myFunction3()" class="dropbtn3">Sweet Spots</button>
-            <div id="mySS" class="dropdown-content">
-                <a href="/Sweet_Spots/Domestic/">Domestic</a>
-            </div>
-        </div>
-        <!-- <a href="/Tierlist">Tierlist</a> -->
-        <!-- <a href="/Travel_Review">Travel Reviews</a> -->
-        <!-- <a href="/Concierge">Concierge</a> -->
-    </nav>
-  </header>
-`}}function openNav(){document.getElementById("mySidenav").style.width="100%"}function closeNav(){document.getElementById("mySidenav").style.width="0"}function openAirline(){document.getElementById("myAirline1").classList.toggle("show")}function openHotel(){document.getElementById("myHotel1").classList.toggle("show")}function openSS(){document.getElementById("mySS1").classList.toggle("show")}function myFunction1(){document.getElementById("myAirline").classList.toggle("show")}function myFunction2(){document.getElementById("myHotel").classList.toggle("show")}function myFunction3(){document.getElementById("mySS").classList.toggle("show")}customElements.define("nav-bar",navBar),window.onclick=function(e){if(!e.target.matches(".dropbtn1")){var i,a=document.getElementsByClassName("dropdown-content");for(i=0;i<a.length;i++){var t=a[i];t.classList.contains("show")&&t.classList.remove("show")}}},window.onclick=function(e){if(!e.target.matches(".dropbtn2")){var i,a=document.getElementsByClassName("dropdown-content");for(i=0;i<a.length;i++){var t=a[i];t.classList.contains("show")&&t.classList.remove("show")}}},window.onclick=function(e){if(!e.target.matches(".dropbtn3")){var i,a=document.getElementsByClassName("dropdown-content");for(i=0;i<a.length;i++){var t=a[i];t.classList.contains("show")&&t.classList.remove("show")}}},window.onclick=function(e){if(!e.target.matches(".dropbtn")){var i,a=document.getElementsByClassName("dropdown-content");for(i=0;i<a.length;i++){var t=a[i];t.classList.contains("show")&&t.classList.remove("show")}}};
+
+            <!-- Desktop Navigation Bar-->
+            <a href="/">
+              <img src="/assets/LogoNav.webp" alt="Worldly Express">
+            </a>
+            <nav>
+              <!--<div class="dropdown">
+                  <button onclick="myFunction()" class="dropbtn">Credit Cards</button>
+                  <div class="dropdown-content">
+                      <a href="/Credit_Card/Amex/">American Express</a>
+                      <a href="/Credit_Card/BoA/">Bank of America</a>
+                      <a href="/Credit_Card/C1/">Capital One</a>
+                      <a href="/Credit_Card/Chase/">Chase</a>
+                      <a href="/Credit_Card/Citi/">Citi</a>
+                      <a href="/Credit_Card/USBank/">US Bank</a>
+                      <a href="/Credit_Card/WF/">Wells Fargo</a>
+                      <a href="/Credit_Card/Other/">Other</a>
+                  </div>
+              </div> -->
+
+              <div class="dropdown">
+                <button onclick="myFunction1()" class="dropbtn">Airlines</button>
+                <div id="myAirline" class="dropdown-content">
+                  <!-- <a href="/Airlines/Air_Canada_Guide/">Air Canada</a>-->
+                  <!-- <a href="/Airlines/Air_France_KLM_Guide">Air France/KLM</a>-->
+                  <!-- <a href="/Airlines/Alaska_Airlines_Guide/">Alaska</a>-->
+                  <!-- <a href="/Airlines/American_Airlines_Guide/">American</a>-->
+                  <a href="/Airlines/Delta_Airlines_Guide/">Delta</a>
+                  <!-- <a href="/Airlines/Frontier_Airlines_Guide/">Frontier</a>-->
+                  <!-- <a href="/Airlines/Qatar_Airways_Guide/">Qatar Airways</a>-->
+                  <!-- <a href="/Airlines/Southwest_Airlines_Guide/">Southwest</a>-->
+                  <!-- <a href="/Airlines/Starlux_Airlines_Guide/">Starlux</a>-->
+                  <!-- <a href="/Airlines/United_Airlines_Guide/">United</a>-->
+                </div>
+              </div>
+
+              <div class="dropdown">
+                <button onclick="myFunction2()" class="dropbtn">Hotels</button>
+                <div id="myHotel" class="dropdown-content">
+                  <!-- <a href="/Hotels/Choice/">Choice</a>-->
+                  <a href="/Hotels/Hilton_Guide/">Hilton</a>
+                  <!-- <a href="/Hotels/Hyatt_Guide/">Hyatt</a>-->
+                  <!-- <a href="/Hotels/IHG_Guide/">IHG</a>-->
+                  <!-- <a href="/Hotels/Marriott_Guide/">Marriott</a>-->
+                  <!-- <a href="/Hotels/Wyndham_Guide/">Wyndham</a>-->
+                </div>
+              </div>
+
+              <div class="dropdown">
+                <button onclick="myFunction3()" class="dropbtn">Sweet Spots</button>
+                <div id="mySS" class="dropdown-content">
+                  <a href="/Sweet_Spots/Domestic/">Domestic</a>
+                </div>
+              </div>
+              <!-- <a href="/Tierlist">Tierlist</a> -->
+              <!-- <a href="/Travel_Review">Travel Reviews</a> -->
+              <!-- <a href="/Concierge">Concierge</a> -->
+            </nav>
+          </header>
+        `;
+  }
+}
+
+// class footBar extends HTMLElement {
+//     connectedCallback() {
+//         this.innerHTML = `
+//         <footer>
+//             <p>© 2023 Worldly Express. All rights reserved.</p>
+//         </footer>
+//         `
+//     }
+// }
+
+customElements.define("nav-bar", navBar);
+
+const sidenav = document.getElementById('sidenav')
+
+function toggleSubMenu(button){
+
+  button.nextElementSibling.classList.toggle('show')
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function openAirline() {
+  document.getElementById("myAirline1").classList.toggle("show");
+}
+
+function openHotel() {
+  document.getElementById("myHotel1").classList.toggle("show");
+}
+
+function openSS() {
+  document.getElementById("mySS1").classList.toggle("show");
+}
+
+function myFunction1() {
+  document.getElementById("myAirline").classList.toggle("show");
+}
+
+function myFunction2() {
+  document.getElementById("myHotel").classList.toggle("show");
+}
+
+function myFunction3() {
+  document.getElementById("mySS").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+  
+
