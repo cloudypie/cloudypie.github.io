db1c385a82ce39f0acc0255425d002ac2294d943
cloudypie <thomasgwu49@gmail.com>
2026-03-28 12:25:08 -0400

    Update google_analytics.js

--- js/google_analytics.js
+++ js/google_analytics.js
@@ -6,6 +6,14 @@
   document.head.appendChild(gtagScript);
 })();
 
+// Your existing Google Analytics code follows...
+(function () {
+  var gtagScript = document.createElement("script");
+  gtagScript.async = true;
+  gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-EFPT5EJVYB";
+  document.head.appendChild(gtagScript);
+})();
+
 // Initialize dataLayer and gtag
 window.dataLayer = window.dataLayer || [];
 function gtag() {
@@ -15,14 +23,3 @@ function gtag() {
 // Configure GA
 gtag("js", new Date());
 gtag("config", "G-EFPT5EJVYB");
-
-
-(function() {
-  var adScript = document.createElement('script');
-  adScript.async = true;
-  adScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3457882836187512";
-  adScript.crossOrigin = "anonymous";
-  
-  // Append to the head of the document
-  document.head.appendChild(adScript);
-})();
